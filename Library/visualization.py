4f72d5dfeef65d17f97a2985e229382a204b92a9
Артём Туйцын <astuytsyn.hse.ru>
2021-04-12 18:17:49 +0300

    обновление 1

--- Library/visualization.py
+++ Library/visualization.py
@@ -5,7 +5,8 @@ Created on Sat Apr  3 17:36:07 2021
 @author: User
 """
 import matplotlib.pyplot as plt
-from analyze import a,b
+from analyze import analyzer 
+
 def add(tipe,kolvo):
     '''
     Parameters
@@ -23,6 +24,11 @@ def add(tipe,kolvo):
     fig = plt.figure()
     a_x = fig.add_axes([0,0,1,1])
     a_x.bar(tipe,kolvo)
-    fig.set_figwidth(12)
+    fig.set_figwidth(16)
     plt.show()
-add(a,b)
+names = ["Type I","Type II","Ability I","Ability II","Hidden Ability","Color"]
+for i in names:
+    print(i,":")
+    a,b= analyzer(i)
+    add(a,b)
+
