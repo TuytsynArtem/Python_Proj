d502f3b64a50873ccb5e22a8bc9b357e4c983f30
Артём Туйцын <astuytsyn.hse.ru>
2021-05-12 21:15:17 +0300

    рабочая версия графического анализа

--- Library/pokemons_reproduction.py
+++ Library/pokemons_reproduction.py
@@ -77,24 +77,50 @@ def analyze(spisok):
     t2 = {}
     for i in range(0,len(spisok)):
         if i%2>0:
-            if t1.get(spisok[i])!=None:
-                t1[spisok[i]]+=1
-            else:
-                t1[spisok[i]]=1
-        else:
-            # if t2[spisok[i]].get(key[,default])!=None:
             if t2.get(spisok[i])!=None:
                 t2[spisok[i]]+=1
             else:
                 t2[spisok[i]]=1
-    print(t1)
-    klv = t1.keys()
-    print(klv)
-    znch = t1.values()
-    print(znch)
-    plt.pie(klv,labels=znch,autopct='%1.1f%%', shadow=True, startangle=90)
+        else:
+            if t1.get(spisok[i])!=None:
+                t1[spisok[i]]+=1
+            else:
+                t1[spisok[i]]=1
+    eggs_data = pd.read_excel('C:/Work/Data/Types.xlsx',sheet_name='Лист1',index_col=0)
+    
+    klv = list(t1.keys())
+    for i in range(0,len(klv)):
+        # print(i)
+        klv[i]=eggs_data["Type_name"][klv[i]]
+        
+    znch = list(t1.values())
+    
+    klv2 = list(t2.keys())
+    for i in range(0,len(klv2)):
+        if  klv2[i]!=0:
+            klv2[i]=eggs_data["Type_name"][klv2[i]]
+        else:
+            klv2[i]="No Type"
+         
+        
+    znch2 = list(t2.values())
+    # print(t1)
+    # print(t2)
+    
+    # print(klv)
+    
+    # print(znch)
+    plt.pie(znch,labels=klv,autopct='%1.1f%%', shadow=True, startangle=90)
     plt.axis('equal')
-    plt.title("Percentage of Different Types of Pokemon")
+    plt.title("Percentage of Different Types I of Pokemon")
+    plt.plot()
+    fig=plt.gcf()
+    fig.set_size_inches(7,7)
+    plt.show()
+    
+    plt.pie(znch2,labels=klv2,autopct='%1.1f%%', shadow=True, startangle=90)
+    plt.axis('equal')
+    plt.title("Percentage of Different Types II of Pokemon")
     plt.plot()
     fig=plt.gcf()
     fig.set_size_inches(7,7)
@@ -112,54 +138,65 @@ def analyze(spisok):
         
         
         
-def Reproduction(raz,dva,baza,kolvo):
-    for i in range(kolvo):
-        typelist = []
-        pokemons = []
-        flag = True
-        if baza["Type I"][raz]!="nan":
-            typelist.append(baza["Type I"][raz])
-        
-        if baza["Type II"][raz]!="nan":
-            typelist.append(baza["Type II"][raz])
-        
-        if baza["Type I"][dva]!="nan":
-            for i in typelist:
-                if i == baza["Type I"][dva]:
-                    flag = False
-            if flag:
-                typelist.append(baza["Type I"][dva])
-                
-                    
-        flag = True
-                
-        if baza["Type II"][dva]!="nan":
+def Reproduction(raz,dva,baza,kolvo):   
+    
+    typelist = []
+    pokemons = []
+    flag = True
+    # print(baza["Type I"][raz])
+    # print(baza["Type II"][raz])
+    # print(baza["Type I"][dva])
+    # print(baza["Type II"][dva])
+    
+    typelist.append(baza["Type I"][raz])
+    
+    if str(baza["Type II"][raz])!="nan":
+        typelist.append(baza["Type II"][raz])
+    
+    if str(baza["Type I"][dva])!="nan":
+        for i in typelist:
+            if i == baza["Type I"][dva]:
+                flag = False
+        if flag:
+            typelist.append(baza["Type I"][dva])
             
-             for i in typelist:
-                if i == baza["Type II"][dva]:
-                    flag = False
-             if flag:
-                 typelist.append(baza["Type II"][dva])
+                
+    flag = True
             
+    if str(baza["Type II"][dva])!="nan":
+        
+         for i in typelist:
+            if i == baza["Type II"][dva]:
+                flag = False
+         if flag:
+             typelist.append(baza["Type II"][dva])
+                 
+    for i in range(kolvo):    
         if len(typelist) == 1:
             pokemons.append(typelist[0])
-            if random.random() == 1: 
+            r = random.randint(0,1)
+            if r == 1: 
                 pokemons.append(typelist[0])
             else:
-                pokemons.append("no Type")
+                pokemons.append(0)
                 
         elif len(typelist) == 2:
-            if random.random() == 1: 
+            r = random.randint(0,1)
+            if  r == 1: 
                 pokemons.append(typelist[0])
             else:
                 pokemons.append(typelist[1])
+                
+                
             r = random.randint(0,2)
+            
             if r == 0: 
                 pokemons.append(typelist[0])
             elif r == 1:
                 pokemons.append(typelist[1])
             elif r == 2:
-                pokemons.append("no Type")
+                pokemons.append(0)
+                
             
             
             
@@ -179,7 +216,7 @@ def Reproduction(raz,dva,baza,kolvo):
             elif r == 2:
                 pokemons.append(typelist[2])
             elif r == 3:
-                pokemons.append("no Type")
+                pokemons.append(0)
             
             
         elif len(typelist) == 4:
@@ -191,8 +228,9 @@ def Reproduction(raz,dva,baza,kolvo):
             elif r == 2:
                 pokemons.append(typelist[2])
             elif r == 3:
-                pokemons.append("no Type")
-            r = random.randint(0,3)
+                pokemons.append(typelist[3])
+                
+            r = random.randint(0,4)
             if r == 0: 
                 pokemons.append(typelist[0])
             elif r == 1:
@@ -201,8 +239,10 @@ def Reproduction(raz,dva,baza,kolvo):
                 pokemons.append(typelist[2])
             elif r == 3:
                 pokemons.append(typelist[3])
-            elif r == 3:
-                pokemons.append("no Type")
+            elif r == 4:
+                pokemons.append(0)
+    # print(typelist)
+    # print(pokemons)
     analyze(pokemons)
 
             
