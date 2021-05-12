4b64591e4ad5f443c3eb2f80a6bbb3abd3c7f66e
Артём Туйцын <astuytsyn.hse.ru>
2021-05-12 22:05:09 +0300

    итоговая версия графического анализа с 2 видами самого анализа

--- Library/pr.py
+++ Library/pr.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue May  4 19:34:32 2021
-
 @author: User
 """
 import pandas as pd
@@ -21,11 +20,9 @@ def proverka(a_1,b_1):
         id of egg group
     b : TYPE Integer
         id of egg group
-
     Returns 1
     -------
     None.
-
     '''
     if eggs_connection[a_1][b_1]==1:
         return 1
@@ -43,7 +40,6 @@ def cheker(id1,id2,d_b):
     Returns True
     -------
     False.
-
     '''
     if(d_b["Egg Group I"][id1]!="-" and d_b["Egg Group II"][id1]!="-"):
         print(d_b["Egg Group II"][id2])
@@ -72,68 +68,6 @@ def cheker(id1,id2,d_b):
                 return True
 
     return False
-
-def avrstats(listok,bz):
-    print("hfersguyvfgjivgbhiewb")
-    HP=0
-    aHP=0
-    Atk=0
-    aAtk=0
-    Def=0
-    aDef=0
-    SpA =0
-    aSpA=0
-    SpD=0
-    aSpD=0
-    Spe=0
-    aSpe=0
-
-    podhodyat = {}
-    for i in range (1,664):
-        j=0
-        while j<len(listok):
-            if bz["Type I"][i]==listok[j] and bz["Type II"][i]==listok[j+1]:
-                if podhodyat.get(i)==None:
-                    podhodyat[i]=0
-            j+=2
-    for i in list(podhodyat):
-        HP += bz["HP"][i]
-        aHP +=1
-        aHP =HP/aHP
-        
-        Atk += bz["Atk"][i]
-        aAtk +=1
-        aAtk =Atk/aAtk
-        
-        Def += bz["Def"][i]
-        aDef +=1
-        aDef =Def/aDef
-        
-        SpA += bz["SpA"][i]
-        aSpA +=1
-        aSpA =SpA/aSpA
-        
-        SpD += bz["SpD"][i]
-        aSpD +=1
-        aSpD =SpD/aSpD
-        
-        Spe += bz["Spe"][i]
-        aSpe +=1
-        aSpe =Spe/aSpe
-    arr = [aHP,aAtk,aDef,aSpA,aSpD,aSpe]
-    arr2 = ["aHP","aAtk","aDef","aSpA","aSpD","aSpe"]
-    
-    fig = plt.figure()
-    a_x = fig.add_axes([0,0,1,1])
-    a_x.bar(arr2,arr)
-    fig.set_figwidth(16)
-    plt.show()
-     
-        
-      
-        
-        
-            
 def analyze(spisok):
     t1 = {}
     t2 = {}
@@ -212,10 +146,10 @@ def Reproduction(raz,dva,baza,kolvo):
     
     typelist.append(baza["Type I"][raz])
     
-    if baza["Type II"][raz]!=0:
+    if str(baza["Type II"][raz])!="nan":
         typelist.append(baza["Type II"][raz])
     
-    if baza["Type I"][dva]!=0:
+    if str(baza["Type I"][dva])!="nan":
         for i in typelist:
             if i == baza["Type I"][dva]:
                 flag = False
@@ -225,7 +159,7 @@ def Reproduction(raz,dva,baza,kolvo):
                 
     flag = True
             
-    if baza["Type II"][dva]!=0:
+    if str(baza["Type II"][dva])!="nan":
         
          for i in typelist:
             if i == baza["Type II"][dva]:
@@ -306,7 +240,6 @@ def Reproduction(raz,dva,baza,kolvo):
     # print(typelist)
     # print(pokemons)
     analyze(pokemons)
-    avrstats(pokemons,baza)
 
             
         
